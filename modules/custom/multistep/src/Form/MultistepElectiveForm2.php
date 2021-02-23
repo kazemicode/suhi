0369ad11159ca190a1d90b3b83333b5353131a98
Sara Kazemi <36528008+kazemicode@users.noreply.github.com>
2021-02-23 01:45:28 -0800

    Update MultistepElectiveForm2.php

--- modules/custom/multistep/src/Form/MultistepElectiveForm2.php
+++ modules/custom/multistep/src/Form/MultistepElectiveForm2.php
@@ -115,6 +115,19 @@ if($grade_level+1 < 12){
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $this->store->set('elective2', $form_state->getValue('elective2'));
+    
+    if($form_state->getValue('elective2') == 93379) {
+      $this->store->set('ss_elective', 95176);
+    }
+    else if( $form_state->getValue('elective2') == 94240) {
+      $this->store->set('ss_elective',0);
+      $this->store->set('macro', 94239);
+    }
+    else {
+      $this->store->set('ss_elective', 0);
+      $this->store->set('macro',0);
+    }
+    
     if($form_state->getTriggeringElement()['#id'] == 'edit-submit') {
       $form_state->setRedirect('multistep.multistep_elective_class_form3');
     }
