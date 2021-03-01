380ddf59b279b35612d81d55024fd70fb399a19d
Sara Kazemi <36528008+kazemicode@users.noreply.github.com>
2021-03-01 10:51:05 -0800

    Update MultistepFinalForm.php

--- modules/custom/multistep/src/Form/MultistepFinalForm.php
+++ modules/custom/multistep/src/Form/MultistepFinalForm.php
@@ -22,7 +22,7 @@ class MultistepFinalForm extends MultistepFormBase {
    * {@inheritdoc}.
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-  debug($this.store);
+  var_dump($form);
 
     $form = parent::buildForm($form, $form_state);
 
