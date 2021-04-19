0418350aabc693c584fde0fd3b848b1dcabec95e
Sara Kazemi <36528008+kazemicode@users.noreply.github.com>
2021-04-19 15:11:50 -0700

    Update MultistepController.php

--- modules/custom/multistep/src/Controller/MultistepController.php
+++ modules/custom/multistep/src/Controller/MultistepController.php
@@ -2,8 +2,6 @@
 <?php
 
 namespace Drupal\multistep\Controller;
-use Drupal\Core\Controller\ControllerBase;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 
 /**
@@ -15,16 +13,6 @@ class MultistepController {
    * Redirect to
    */
   public function deactivateForm() {
-   // $base_url = 'https://kazemicode.org/suhi';
-
-   // $path = '/courses/closed';
-
-   // $response = new RedirectResponse($base_url . $path);
-  //  $response->send();
-    return new Response('Preregistration is now closed!');
-  }
-  
-
-
-
+        return new Response('RAWWWWWR!');
+    }
 }
